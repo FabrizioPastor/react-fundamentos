6602c60834db6cceac85fb97354246f5adcb8d85
fabriziopastor <fabriziopastors@outlook.com>
2022-09-10 17:43:00 -0500

    creando componente funional

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = <h1>Hola Mundo!</h1>
+const App = <h1>Hola Mundo!</h1> 
+
+const TarjetaFruta = () => {
+    return (
+        <div>
+            <h3>Titulo</h3>
+            <hr/>
+            <p>Descripcion</p>
+        </div>
+    )
+}
 
 ReactDOM.render(App, document.getElementById('root'))
