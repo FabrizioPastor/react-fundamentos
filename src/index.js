e4c0de5ed650e260bcb3fc73237acb4f43b02ac2
fabriziopastor <fabriziopastors@outlook.com>
2022-09-10 19:55:03 -0500

    cambiando identación de 4 a 2 espacios

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 class TarjetaFruta extends React.Component {
-    /**Al usuar clases para crear componentes, las "props" no llegan através
-     * del método; si no, llegan usando la palabra reservada this
-     */
-    render () {
-        return ( 
-            <div>
-                <h3>{this.props.name}</h3>
-                <hr/>
-                <p> {this.props.price}</p>
-            </div>
-        )
-    }
+  /**Al usuar clases para crear componentes, las "props" no llegan através
+   * del método; si no, llegan usando la palabra reservada this
+   */
+  render() {
+    return (
+      <div>
+        <h3>{this.props.name}</h3>
+        <hr />
+        <p> {this.props.price}</p>
+      </div>
+    );
+  }
 }
 
 /**Las props siempre son el primer parámetro que se le pasa
  * a los componentes funcionales.
  */
 const TarjetaFruta = (props) => {
-    return (
-        <div>
-            <h3>{props.name}</h3>
-            <hr/>
-            <p> {props.price}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>{props.name}</h3>
+      <hr />
+      <p> {props.price}</p>
+    </div>
+  );
+};
 
 const App = () => (
-    <div>
-        <TarjetaFruta name= 'Naranja' price={ 2.00 }/>
-        <TarjetaFruta name= 'Granadilla' price={ 5.50 }/>
-        <TarjetaFruta name= 'Palta' price={ 7.99 }/>
-    </div>
-)
+  <div>
+    <TarjetaFruta name="Naranja" price={2.0} />
+    <TarjetaFruta name="Granadilla" price={5.5} />
+    <TarjetaFruta name="Palta" price={7.99} />
+  </div>
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
